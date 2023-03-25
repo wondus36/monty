d43c67f03548f0158801090a2990facc0965c156
wondu shiferaw <33760699+wondus36@users.noreply.github.com>
2023-03-25 13:00:40 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,4 @@
 #include "monty.h"
-#define  _GNU_SOURCE
-#include <stdio.h>
 bus_t bus = {NULL, NULL, NULL, 0};
 /**
 * main - monty code interpreter
@@ -32,7 +30,7 @@ int main(int argc, char *argv[])
 	while (read_line > 0)
 	{
 		content = NULL;
-		read_line =  getline(&string, &size, stdin);
+		read_line = getline(&content, &size, file);
 		bus.content = content;
 		counter++;
 		if (read_line > 0)
